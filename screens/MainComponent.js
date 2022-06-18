368e43ddcabc410b85aa000f2cfd594308cd3c0d
Rebecca Basquez <beckycb78@gmail.com>
2022-06-18 11:57:34 -0500

    Week 1 Workshop Assignment

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -5,6 +5,8 @@ import Constants from 'expo-constants';
 import { createStackNavigator } from '@react-navigation/stack';
 import DirectoryScreen from "./DirectoryScreen";
 import CampsiteInfoScreen from "./CampsiteInfoScreen";
+import AboutScreen from './AboutScreen';
+import ContactScreen from './ContactScreen';
 
 const Drawer = createDrawerNavigator();
 
@@ -52,6 +54,33 @@ const DirectoryNavigator = () => {
     )
 }
 
+const AboutNavigator = () => {
+    const Stack = createStackNavigator();
+
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen 
+                name='About'
+                component={AboutScreen}                
+            />
+        </Stack.Navigator>
+    )
+}
+
+const ContactNavigator = () => {
+    const Stack = createStackNavigator();
+
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen 
+                name='Contact'
+                component={ContactScreen}
+                options = {{ title: 'Contact Us'}}
+            />
+        </Stack.Navigator>
+    )
+}
+
 const Main = () => {
     
     return (
@@ -75,6 +104,15 @@ const Main = () => {
                    name='Directory'
                    component={DirectoryNavigator}
                    options={{ title: 'Directory' }} 
+                />
+                <Drawer.Screen 
+                   name='About'
+                   component={AboutNavigator} 
+                />
+                <Drawer.Screen 
+                   name='Contact'
+                   component={ContactNavigator}
+                   options={{ title: 'Contact Us' }} 
                 />
             </Drawer.Navigator>            
         </View>
