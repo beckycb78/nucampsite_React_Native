17af9ea13cd9f99cef523221974bcac1fd2468f8
Rebecca Basquez <beckycb78@gmail.com>
2022-06-23 23:48:53 -0500

    Custom Side Drawer

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,6 +1,6 @@
 import HomeScreen from './HomeScreen';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { Platform, View, StyleSheet } from 'react-native';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
+import { Platform, View, StyleSheet, Text, Image } from 'react-native';
 import Constants from 'expo-constants';
 import { createStackNavigator } from '@react-navigation/stack';
 import DirectoryScreen from "./DirectoryScreen";
@@ -8,6 +8,7 @@ import CampsiteInfoScreen from "./CampsiteInfoScreen";
 import AboutScreen from './AboutScreen';
 import ContactScreen from './ContactScreen';
 import { Icon } from 'react-native-elements';
+import logo from '../assets/images/logo.png';
 
 const Drawer = createDrawerNavigator();
 
@@ -122,6 +123,21 @@ const ContactNavigator = () => {
     )
 }
 
+const CustomDrawerContent = (props) => (    
+    <DrawerContentScrollView {...props}>
+        <View style={styles.drawerHeader}>
+            <View style={{ flex: 1 }}>
+                <Image source={logo} style={styles.drawerImage} />
+            </View>
+            <View style={{ flex: 2 }}>
+                <Text style={styles.drawerHeaderText}>NuCamp</Text>
+            </View>
+        </View>
+        <DrawerItemList {...props} labelStyle={{ fontWeight:'bold'}} />
+    </DrawerContentScrollView>
+);
+
+
 const Main = () => {
     
     return (
@@ -135,6 +151,7 @@ const Main = () => {
             <Drawer.Navigator
                 initialRouteName='Home'
                 drawerStyle={{ backgroundColor: '#CEC8FF' }}
+                drawerContent= {CustomDrawerContent}
             >
                 <Drawer.Screen 
                     name='Home'
@@ -207,6 +224,24 @@ const Main = () => {
 };
 
 const styles = StyleSheet.create({
+    drawerHeaderText: {
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    drawerImage: {
+        margin: 10,
+        height: 60,
+        width: 60
+    },
+    drawerHeader: {
+        backgroundColor: '#5637DD',
+        height: 140,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row'
+    },
     stackIcon: {
         marginLeft: 10,
         color: '#fff',
