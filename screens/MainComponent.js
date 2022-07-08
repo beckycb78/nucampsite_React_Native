8ab53dc1af5ee716b29f495dac1d324c60758789
Rebecca Basquez <beckycb78@gmail.com>
2022-07-08 00:45:59 -0500

    Secure Store

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -17,6 +17,7 @@ import { fetchPromotions } from '../features/promotions/promotionsSlice';
 import { fetchComments } from '../features/comments/commentsSlice';
 import ReservationScreen from './ReservationComponent';
 import FavoritesScreen from './FavoritesScreen';
+import LoginScreen from './LoginScreen';
 
 const Drawer = createDrawerNavigator();
 
@@ -179,6 +180,30 @@ const FavoritesNavigator = () => {
     )
 }
 
+const LoginNavigator = () => {
+    const Stack = createStackNavigator();
+
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen 
+                name='Login'
+                component={LoginScreen}
+                options={({ navigation }) => ({
+                    headerLeft: () => (
+                        <Icon 
+                            name='sign-in'
+                            type='font-awesome'
+                            iconStyle={styles.stackIcon}
+                            onPress={() => navigation.toggleDrawer()}
+                        />
+                    )
+                })}
+            />
+        </Stack.Navigator>
+    )
+}
+
+
 const CustomDrawerContent = (props) => (    
     <DrawerContentScrollView {...props}>
         <View style={styles.drawerHeader}>
@@ -217,6 +242,21 @@ const Main = () => {
                 drawerStyle={{ backgroundColor: '#CEC8FF' }}
                 drawerContent= {CustomDrawerContent}
             >
+                <Drawer.Screen 
+                    name='Login'
+                    component={LoginNavigator}
+                    options={{ 
+                        drawerIcon: ({ color }) => (
+                            <Icon 
+                                name='sign-in'
+                                type='font-awesome'
+                                size={24}
+                                iconStyle={{ width: 24}}
+                                color={color}
+                            />
+                        )
+                    }}
+                />
                 <Drawer.Screen 
                     name='Home'
                     component={HomeNavigator}
